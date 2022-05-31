67b8404e45db053c587bb0eca31a437ed37775e1
Corey Coleman <coreycoleman1339@gmail.com>
2022-05-30 17:29:07 -0700

    netlify deploy

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -14,11 +14,11 @@ export default function Home() {
 
     return (
 
-        <div className='home d-flex align-items-center  justify-content-center'>
+        <div className='home d-flex align-items-lg-center pt-5 px-5  justify-content-center'>
             <Container >
-                <Row>
-                    <Col>
-                        <h1>
+                <Row >
+                    <Col className='test'>
+                        <h1 className='intro'>
                             Hi, I'm <span className='name'>Corey</span><br />
                             <Typewriter
                                 options={{
@@ -31,13 +31,13 @@ export default function Home() {
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
+                    <Col className='px-0 py-3'>
                         <h3 className='tagline'>Front End Developer/ Programming Enthusiast </h3>
                     </Col>
                 </Row>
                 <Row>
-                    <div>
-                        <Button className='contact-button' >Contact Me</Button>
+                    <div className='d-flex justify-content-center justify-content-lg-start '>
+                        <Button className='contact-button my-5' >Contact Me</Button>
                     </div>
                 </Row>
             </Container>
