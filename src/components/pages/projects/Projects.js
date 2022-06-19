f29f85029dbdede19f03dd9a62df9a5480aa6076
Corey Coleman <coreycoleman1339@gmail.com>
2022-06-19 13:38:07 -0700

    autoprefixer bug

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -16,7 +16,7 @@ const ProjectCard = ({line, reverse, title, img, description, link, handleShow }
             <Col className=' col-12 col-md-6 custom-border' >
                 <span onClick={handleShow} href={link} target="_blank" className='animate overflow-hidden   mb-0 d-flex justify-content-center'>
                    <span className='image-wrapper'></span>
-                    <img className='pic' src={img} alt='project image' />
+                    <img className='pic' src={img} alt='Sample Projects Preview' />
                 </span>
             </Col>
             <Col className=' col-12 col-md-6'>
@@ -61,33 +61,3 @@ export default function Projects() {
 
 
 
-{/* <Container  id='projects'>
-<VideoModal show={show} setShow={setShow} handleClose={handleClose} handleShow={handleShow} />
-<h2 className='my-5'>Projects</h2>
-<Row >
-    <Col className='col-12 col-lg-4'  >
-        <ProjectCard handleShow={handleShow} title='Movie Swiper' img='https://picsum.photos/300' description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/MovieSwiper' />
-    </Col>
-    <Col className='col-12 col-lg-4'  >
-        <ProjectCard handleShow={handleShow} title='News App' img='https://picsum.photos/300' description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/NewsApp' />
-    </Col>
-    <Col className='col-12 col-lg-4'  >
-        <ProjectCard handleShow={handleShow} title='Weather App' img='https://picsum.photos/300' description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/Weather-App' />
-    </Col>
-</Row>
-</Container> */}
-
-/* 
-<>
-
-<span onClick={handleShow} href={link} target="_blank" className='animate mx-auto overflow-hidden mb-0 d-flex justify-content-center'>
-    <span className='shadow-container'>
-    </span>
-    <img className='pic' src={img} />
-
-</span>
-<a href={link} target="_blank" className='animate mx-auto overflow-hidden mb-0 d-flex justify-content-center'>
-    <h3>{title}</h3>
-</a>
-<p className=''>{description}</p>
-</> */
