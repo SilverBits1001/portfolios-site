16e5ac3bf6bcd5815fce84391276b22a42fcd750
Corey Coleman <coreycoleman1339@gmail.com>
2022-06-22 19:53:38 -0700

    updated demos

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
+import MovieSwiperImg from '../../../assets/images/MovieSwiper/movieappdemo.png'
+import NewsAppDemo from '../../../assets/images/MovieSwiper/newsappdemo.png'
+import WeatherDemo from '../../../assets/images/MovieSwiper/weatherdemo.png'
+import GreenGuideDemo from '../../../assets/images/MovieSwiper/greenguidedemo.png'
+
+
 import './Projects.scss'
 import VideoModal from './VideoModal'
 
 
 
-const ProjectCard = ({line, reverse, title, img, description, link, handleShow }) => {
+const ProjectCard = ({ line, reverse, title, img, description, link, handleShow }) => {
 
     const direction = reverse ? 'flex-row-reverse' : 'flex-row'
 
@@ -15,12 +21,12 @@ const ProjectCard = ({line, reverse, title, img, description, link, handleShow }
         <Row className={`d-flex ${direction} project-row `}>
             <Col className=' col-12 col-md-6 custom-border' >
                 <span onClick={handleShow} href={link} target="_blank" className='animate overflow-hidden   mb-0 d-flex justify-content-center'>
-                   <span className='image-wrapper'></span>
+                    <span className=''></span>
                     <img className='pic' src={img} alt='Sample Projects Preview' />
                 </span>
             </Col>
             <Col className=' col-12 col-md-6'>
-                <a href={link} target="_blank"  rel="noreferrer" className='animate overflow-hidden mb-0 d-flex justify-content-center'>
+                <a href={link} target="_blank" rel="noreferrer" className='animate overflow-hidden mb-0 d-flex justify-content-center'>
                     <h3>{title}</h3>
                 </a>
                 <p className='project-description '>{description}</p>
@@ -44,16 +50,16 @@ export default function Projects() {
             <VideoModal show={show} setShow={setShow} handleClose={handleClose} handleShow={handleShow} />
             <h2 className='text-center my-5'>Projects</h2>
             <div className='row-wrapper left top mx-md-5'>
-                <ProjectCard reverse={false} handleShow={handleShow} title='Movie Swiper App' img='https://picsum.photos/id/1/250' description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/Weather-App' />
+                <ProjectCard reverse={false} handleShow={handleShow} title='Movie Swiper App' img={MovieSwiperImg} description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/Weather-App' />
             </div>
             <div className='row-wrapper top right mx-md-5'>
-                <ProjectCard l reverse={true} handleShow={handleShow} title='Weather App' img='https://picsum.photos/id/10/250' description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/Weather-App' />
+                <ProjectCard l reverse={true} handleShow={handleShow} title='Weather App' img={WeatherDemo} description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/Weather-App' />
             </div>
             <div className='row-wrapper top  left mx-md-5'>
-                <ProjectCard  reverse={false} handleShow={handleShow} title='News App' img='https://picsum.photos/id/15/250' description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/Weather-App' />
+                <ProjectCard reverse={false} handleShow={handleShow} title='News App' img={NewsAppDemo} description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/Weather-App' />
             </div>
             <div className='row-wrapper top bottom right mx-md-5'>
-                <ProjectCard  reverse={true} handleShow={handleShow} title='The Green Guide ' img='https://picsum.photos/id/20/250' description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/Weather-App' />
+                <ProjectCard reverse={true} handleShow={handleShow} title='The Green Guide ' img={GreenGuideDemo} description='This is a brief description of the listed project explaining what it does possibly.' link='https://github.com/SilverBits1001/Weather-App' />
             </div>
         </Container>
     )
